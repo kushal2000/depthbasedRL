5f0d7faa709bbc770cac44ffc268080525e9bb1d
Tyler Lum <tylergwlum@gmail.com>
2026-04-24 18:03:35 -0700

    Skip RGBD bright preflight heuristic

--- scripts/cluster/codex_distill_online_with_preflight.sh
+++ scripts/cluster/codex_distill_online_with_preflight.sh
@@ -109,7 +109,7 @@ for N in "${ENV_COUNTS[@]}"; do
     --teacher_config "$TEACHER_CONFIG" \
     --run_dir "$PREFLIGHT_DIR" \
     "${PREFLIGHT_IMAGE_ARGS[@]}"; then
-    if [[ "$STUDENT_INPUT" == "camera" && "${STUDENT_MODALITY:-}" != "depth" ]]; then
+    if [[ "$STUDENT_INPUT" == "camera" && "${STUDENT_MODALITY:-}" != "depth" && "${STUDENT_MODALITY:-}" != "rgbd" ]]; then
       if ! python - "$PREFLIGHT_DIR/policy_image_stats.csv" "$IMAGE_PREFLIGHT_BRIGHT_FRAC_MAX" <<'PY'
 import csv
 import sys
